fae768ee9992321ba09da01821d5e3c95836d803
castetus <kboikov@mail.ru>
2021-10-20 23:23:40 +0300

    fix: multiple bugs

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const action = document.querySelector('.action');
+const action = document.querySelector('.fetch');
 const templateImageCard = document.querySelector('#image');
 const templateImagePopup = document.querySelector('#popup-image');
 const container = document.querySelector('.images');
@@ -10,7 +10,7 @@ const popupContainer = document.querySelector('.popup .content');
 const popupClose = document.querySelector('.popup .action');
 const loader = document.querySelector('.loader');
 
-const MAX_PAGE_IAMGES = 34;
+const MAX_PAGE_IMAGES = 34;
 let loaderTimeout;
 
 /**
@@ -40,7 +40,7 @@ const getPictures = function (page = 1, limit = 10) {
  * и вызывает ф-цию для отрисовки картинки в попапе
  * @param {number} id
  */
-const getPictureInfo = function (id = 0) {
+const getPictureInfo = function (id) {
     showLoader();
     fetch(`https://picsum.photos/id/${id}/info`)
         .then(function (response) {return response.json()})
@@ -62,7 +62,7 @@ const showLoader = function () {
 const hideLoader = function () {
     loaderTimeout = setTimeout(function () {
         loader.style.visibility = 'hidden';
-        loaderTimeout.clearTimeout();
+        // loaderTimeout.clearTimeout();
     }, 700);
 }
 
@@ -90,15 +90,13 @@ const renderPictures = function (list) {
     if (!list.length) {
         throw Error(`Pictures not defined. The list length: ${list.length}`);
     }
-
-    const clone = templateImageCard.content.cloneNode(true);
     const fragment = document.createDocumentFragment();
 
-    list.forEach(function (element) {
-        const link = clone.querySelector('a');
-
-        link.href = element.url;
-        link.dataset.id = element.id;
+    list.forEach(function (element) {  
+      const clone = templateImageCard.content.firstElementChild.cloneNode(true);
+      
+        clone.href = element.url;
+        clone.dataset.id = element.id;
 
         const image = clone.querySelector('img');
         image.src = cropImage(element.download_url, 5);
@@ -152,10 +150,10 @@ const togglePopup = function () {
 const actionHandler = function (evt) {
     evt.preventDefault();
     const nextPage = evt.currentTarget.dataset.page;
-    evt.currentTarget.dataset.page = nextPage + 1;
+    evt.currentTarget.dataset.page = +nextPage + 1;
 
-    if (nextPage > MAX_PAGE_IAMGES) {
-        console.warn(`WARN: You are trying to call a page that exceeds ${MAX_PAGE_IAMGES}`);
+    if (nextPage > MAX_PAGE_IMAGES) {
+        console.warn(`WARN: You are trying to call a page that exceeds ${MAX_PAGE_IMAGES}`);
         evt.currentTarget.disabled = true;
     } else {
         getPictures(nextPage);
@@ -172,7 +170,8 @@ const imageHandler = function (evt) {
     evt.preventDefault();
 
     if (evt.target.closest('a')) {
-        getPictureInfo(evt.target.dataset.id);
+      const parent = evt.target.closest('a')
+        getPictureInfo(parent.dataset.id);
     }
 }
 
